a3a4b95251dcc0d6cdd8cab235d80f50e11614e4
Butty256 <butty.main@gmail.com>
2020-11-03 10:13:21 +0900

    int only

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define STR_LENGTH 1024
 enum token {NUMBER = 1, OPERATOR = 2, LEFTPAREN = 3, RIGHTPAREN = 4};
@@ -14,89 +15,89 @@ struct count
 	int rightparen;
 };
 
+void strrep(char str[], const char *bef, const char *aft);
+int isoperator(char c);
+char *getoperator(char *str);
+int calc(char *str, int i);
+int eval(char *str);
+
 int isoperator(char c)
 {
 	if (c == '+' || c == '-' || c == '*' || c == '/') return 1;
 	return 0;
 }
 
-char *getoperator(char *exp)
+char *getoperator(char *str)
 {
 	int i, count = 0;
 
-	for (i = 0; (count != 0 || exp[i] != ')') && exp[i] != '\0'; i++)
+	for (i = 0; (count != 0 || str[i] != ')') && str[i] != '\0'; i++)
 	{
-		if (count == 0 && isoperator(exp[i])) return &exp[i];
-		else if (exp[i] == '(') count++;
-		else if (exp[i] == ')') count--;
+		if (count == 0 && isoperator(str[i])) return &str[i];
+		else if (str[i] == '(') count++;
+		else if (str[i] == ')') count--;
 	}
 	return NULL;
 }
 
 int calc(char *str, int i)
 {
-	int j = i, k,  tmp = 0, a, b;
-	//式中を配列で操作するための添え字j
-	//文字列置換字に使うループ用変数k
-	//式の値を保存する変数tmp
-	//式の左辺、右辺の値を保存するa,b
+	int j, k,  tmp = 0, a, b;
 
 	char exp[STR_LENGTH], val[10];
-	//演算している式を保存する文字列exp
-	//値を文字列として使用するval
 
 	char *pl, *pr, *next;
 	//左辺の値の先頭pl、右辺の値の終端pr
 	//右辺の先の演算子をチェックする時に使うnext
 
-	while(1)
-	{   //左辺の値を検索する
+	// 左辺 while いる？
+	j = i;
+	while (1)
+	{
 		j--;
 		if (str[j] == ')')
-		{ //左辺終端がカッコとじである場合
+		{
 			while (str[j] != '(')
 			{
-				//カッコまで進めて、evalで左辺値を求める
 				j--;
 				if (j == -1) exit(EXIT_FAILURE);
 			}
-			pl = str + j;   //左辺値の先頭を保存
+			pl = str + j;
 			a = eval(pl);
-			//左辺の値は得たので、ループを抜ける
 			break;
-
 		}
 		else if (isdigit(str[j]))
-		{	//数だった場合
+		{
 			while (isdigit(str[j - 1]))
-			{	//数の先頭までずらしてatoi
+			{
 				j--;
 				if (j == 0) exit(EXIT_FAILURE);
 			}
-			pl = str + j;   //左辺の先頭を保存
-			a = atoi(pl);   //値を取得して、ループを抜ける
+			pl = str + j;
+			a = atoi(pl);
 			break;
 		}
 		else if (str[j] == '(')
-		{  //演算子が式先頭のマイナスだった場合
-			a = 0;  //左辺値は0として演算すればよい
+		{
 			pl = str + j + 1;
+			a = 0;
 			break;
 		}
 	}
 
-	j = i;  //今度は右辺の値を格納する
+	// 右辺 while いる？
+	j = i;
 	while (str[j] != ')')
-	{  //最大で右辺終端まで
+	{
 		j++;
 		if (str[j] == '(')
-		{ //右辺がカッコである場合
-			pr = str + j;   //右辺値の値先頭アドレスを保存
+		{
+			pr = str + j;
 			if (str[i] == '+' || str[i] == '-')
-			{	//現在の式の演算子が加減である場合、先の乗除をチェックする
+			{
 				if ((next = getoperator(pr)) != NULL)
-				{  //右辺の先に乗除がないか
-					switch(*next)
+				{
+					switch (*next)
 					{
 						case '*':
 						case '/': b = calc(pr, next - pr); break;
@@ -108,11 +109,8 @@ int calc(char *str, int i)
 				else b = eval(pr);
 			}
 			else b = eval(pr);
-
-			//右辺値終端を式の終わりと見るため
-			//右辺値の終端を探す
 			if (str[j] == '(')
-			{ //右辺先頭がカッコである場合
+			{
 				while (str[j] != ')')
 				{
 					j++;
@@ -121,7 +119,6 @@ int calc(char *str, int i)
 			}
 			else if (isdigit(str[j])) while (isdigit(str[j + 1])) j++;
 			else exit(EXIT_FAILURE);
-			//このときstr + jは右辺値の終端を指す
 			break;
 		}
 		else if (isdigit(str[j]))
@@ -131,7 +128,7 @@ int calc(char *str, int i)
 			{
 				if ((next = getoperator(pr)) != NULL)
 				{  //同じく右辺の先の乗除をチェック
-					switch(*next)
+					switch (*next)
 					{
 						case '*':
 						case '/': b = calc(pr, next - pr); break;
@@ -143,7 +140,6 @@ int calc(char *str, int i)
 				else b = atoi(pr);   //atoiで数を求める
 			}
 			else b = atoi(pr);   //自身の演算が乗除であった場合、先に計算
-
 			if (str[j] == '(')
 			{ //右辺先頭がカッコである場合
 				while (str[j] != ')')
@@ -159,17 +155,12 @@ int calc(char *str, int i)
 	}
 
 	//右辺、左辺の値を受け取ったので演算する
-	switch(str[i])
+	switch (str[i])
 	{
 		case '+': tmp = a + b; break;
 		case '-': tmp = a - b; break;
 		case '*': tmp = a * b; break;
-		case '/':
-		{
-			if (b == 0) exit(EXIT_FAILURE);
-			else tmp = a / b;
-			break;
-		}
+		case '/': if (b == 0) exit(EXIT_FAILURE); else tmp = a / b; break;
 		default: break;
 	}
 
@@ -185,8 +176,8 @@ int calc(char *str, int i)
 
 	if (tmp >= 0) sprintf(val, "%d", tmp);
 	else sprintf(val, "(%d)", tmp);
+	strrep(str, exp, val);
 
-	mystrrep(str, exp, val);	//置換
 	return tmp;
 }
 
@@ -232,7 +223,7 @@ void strrep(char str[], const char *bef, const char *aft)
 void init(const char in[], char out[])
 {
 	sprintf(out, "(%s)", in);
-	while (strchr(out, " ")) strrep(out, " ", "");
+	while (strchr(out, ' ')) strrep(out, " ", "");
 }
 
 int main(int argc, char **argv)
@@ -246,6 +237,7 @@ int main(int argc, char **argv)
 	}
 
 	init(argv[1], str);
+	printf("expression:%s\nanswer:%d\n", argv[1], eval(str));
 
 	return 0;
 }
