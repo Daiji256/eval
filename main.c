c9637fb532a5fa177f3b92182eeace5953777192
Butty256 <butty.main@gmail.com>
2020-11-03 11:14:28 +0900

    edit loop

--- main.c
+++ main.c
@@ -21,6 +21,12 @@ char *getoperator(char *str);
 double calc(char *str, int i);
 double eval(char *str);
 
+int isnumber(char c)
+{
+	if (isdigit(c) || c == '.') return 1;
+	return 0;
+}
+
 int isoperator(char c)
 {
 	if (c == '+' || c == '-' || c == '*' || c == '/') return 1;
@@ -46,117 +52,101 @@ double calc(char *str, int i)
 	double tmp = 0, a, b;
 	printf("calc: str = %s\n", str);
 
-	char exp[STR_LENGTH], val[50];
+	char str2[STR_LENGTH], val[50];
 
 	char *pl, *pr, *next;
-	//左辺の値の先頭pl、右辺の値の終端pr
-	//右辺の先の演算子をチェックする時に使うnext
 
-	// 左辺 while いる？
-	j = i;
-	while (1)
+	// 左辺
+	j = i - 1;
+	if (str[j] == ')')
 	{
-		j--;
-		if (str[j] == ')')
+		while (str[j] != '(')
 		{
-			while (str[j] != '(')
-			{
-				j--;
-				if (j == -1) exit(EXIT_FAILURE);
-			}
-			pl = str + j;
-			a = eval(pl);
-			break;
+			j--;
+			if (j == -1) exit(EXIT_FAILURE);
 		}
-		else if (isdigit(str[j]) || str[j] == '.')
+		pl = str + j;
+		a = eval(pl);
+	}
+	else if (isnumber(str[j]))
+	{
+		while (isnumber(str[j - 1]))
 		{
-			while (isdigit(str[j - 1]) || str[j - 1] == '.')
-			{
-				j--;
-				if (j == -1) exit(EXIT_FAILURE);
-			}
-			pl = str + j;
-			a = atof(pl);
-			break;
+			j--;
+			if (j == -1) exit(EXIT_FAILURE);
 		}
-		else if (str[j] == '(')
-		{
-			pl = str + j + 1;
-			a = 0;
-			break;
-		}
+		pl = str + j;
+		a = atof(pl);
+	}
+	else if (str[j] == '(')
+	{
+		pl = str + j + 1;
+		a = 0;
 	}
 
-	// 右辺 while いる？
-	j = i;
-	while (str[j] != ')')
+	// 右辺
+	j = i + 1;
+	if (str[j] == '(')
 	{
-		j++;
-		if (str[j] == '(')
+		pr = str + j;
+		if (str[i] == '+' || str[i] == '-')
 		{
-			pr = str + j;
-			if (str[i] == '+' || str[i] == '-')
+			if ((next = getoperator(pr)) != NULL)
 			{
-				if ((next = getoperator(pr)) != NULL)
+				switch (*next)
 				{
-					switch (*next)
-					{
-						case '*':
-						case '/': b = calc(pr, next - pr); break;
-						case '+':
-						case '-': b = eval(pr); break;
-						default: break;
-					}
+					case '*':
+					case '/': b = calc(pr, next - pr); break;
+					case '+':
+					case '-': b = eval(pr); break;
+					default: break;
 				}
-				else b = eval(pr);
 			}
 			else b = eval(pr);
-			if (str[j] == '(')
-			{
-				while (str[j] != ')')
-				{
-					j++;
-					if (str[j] == '\0') exit(EXIT_FAILURE);
-				}
-			}
-			else if (isdigit(str[j]) || str[j] == '.') while (isdigit(str[j + 1]) || str[j + 1] == '.') j++;
-			else exit(EXIT_FAILURE);
-			break;
 		}
-		else if (isdigit(str[j]) || str[j] == '.')
-		{	//右辺値が数の場合
-			pr = str + j;   //右辺値先頭を保存
-			if (str[i] == '+' || str[i] == '-')
+		else b = eval(pr);
+		if (str[j] == '(')
+		{
+			while (str[j] != ')')
 			{
-				if ((next = getoperator(pr)) != NULL)
-				{  //同じく右辺の先の乗除をチェック
-					switch (*next)
-					{
-						case '*':
-						case '/': b = calc(pr, next - pr); break;
-						case '+':
-						case '-': b = atof(pr); break;
-						default: break;
-					}
+				j++;
+				if (str[j] == '\0') exit(EXIT_FAILURE);
+			}
+		}
+		else if (isnumber(str[j])) while (isnumber(str[j + 1])) j++;
+		else exit(EXIT_FAILURE);
+	}
+	else if (isnumber(str[j]))
+	{
+		pr = str + j;
+		if (str[i] == '+' || str[i] == '-')
+		{
+			if ((next = getoperator(pr)) != NULL)
+			{
+				switch (*next)
+				{
+					case '*':
+					case '/': b = calc(pr, next - pr); break;
+					case '+':
+					case '-': b = atof(pr); break;
+					default: break;
 				}
-				else b = atof(pr);
 			}
 			else b = atof(pr);
-			if (str[j] == '(')
-			{ //右辺先頭がカッコである場合
-				while (str[j] != ')')
-				{
-					j++;
-					if (str[j] == '\0') exit(EXIT_FAILURE);
-				}
-			}
-			else if (isdigit(str[j]) || str[j] == '.') while (isdigit(str[j + 1]) || str[j + 1] == '.') j++;
-			else exit(EXIT_FAILURE);
-			break;
 		}
+		else b = atof(pr);
+		if (str[j] == '(')
+		{
+			while (str[j] != ')')
+			{
+				j++;
+				if (str[j] == '\0') exit(EXIT_FAILURE);
+			}
+		}
+		else if (isnumber(str[j])) while (isnumber(str[j + 1])) j++;
+		else exit(EXIT_FAILURE);
 	}
 
-	//右辺、左辺の値を受け取ったので演算する
 	switch (str[i])
 	{
 		case '+': tmp = a + b; break;
@@ -166,19 +156,12 @@ double calc(char *str, int i)
 		default: break;
 	}
 
-	//計算の終わった式を消去する
-	//左辺値の先頭が式の始まり、右辺値の計算より、str + jが終端である
-	k = 0;
-	while (pl + k <= str + j)
-	{   //まずは演算した式をexpにコピー
-		exp[k] = pl[k];
-		k++;
-	}
-	exp[k] = '\0';
+	for (k = 0; pl + k <= str + j; k++) str2[k] = pl[k];
+	str2[k] = '\0';
 
 	if (tmp >= 0) sprintf(val, "%f", tmp);
 	else sprintf(val, "(%f)", tmp);
-	strrep(str, exp, val);
+	strrep(str, str2, val);
 
 	return tmp;
 }
@@ -187,10 +170,7 @@ double eval(char *str)
 {
 	int i, count;
 	double tmp;
-	//ループ用変数i、演算結果を保存するtmp、カッコ のカウント用count
 	char *p;
-	//ポインタ操作用p
-	printf("eval: str = %s\n", str);
 
 	count = 0;
 	p = str + 1;
